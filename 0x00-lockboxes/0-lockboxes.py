372a313efb84c370d3e35162237b43e0a185df3b
Juansu01 <thejuank0@hotmail.com>
2021-12-08 13:09:09 -0500

    Adding an approach with less code

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -6,22 +6,12 @@ def canUnlockAll(boxes):
     """Takes in a list of boxes and returns True if all boxes can be unlocked,
     otherwise it returns False."""
 
-    unlocked_indexes = []
+    unlocked_indexes = [0]
     for i, box in enumerate(boxes):
         if i == 0:
             unlocked_indexes.extend(boxes[0])
             continue
-        if i != 0 and i in unlocked_indexes:
-            unlocked_indexes.extend(box)
-    mylist = list(dict.fromkeys(unlocked_indexes))
-    mylist = sorted(mylist)
-    if 0 not in unlocked_indexes:
-        mylist.insert(0, 0)
-    for i in mylist:
-        for j in boxes[i]:
-            if j not in mylist:
-                mylist.append(j)
-    mylist = sorted(mylist)
-    if len(mylist) != len(boxes):
-        return False
-    return True
+        for key in box:
+            if key not in unlocked_indexes and box != boxes[len(boxes) - 1]:
+                unlocked_indexes.append(key)
+    return (len(unlocked_indexes) == len(boxes))
