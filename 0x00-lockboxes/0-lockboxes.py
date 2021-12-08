a80bb8720cded8093c9f2733ec79c2c0c91fe959
Juansu01 <thejuank0@hotmail.com>
2021-12-08 13:13:24 -0500

    Iterating over keys and expanding list as we access different boxes

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -7,11 +7,8 @@ def canUnlockAll(boxes):
     otherwise it returns False."""
 
     unlocked_indexes = [0]
-    for i, box in enumerate(boxes):
-        if i == 0:
-            unlocked_indexes.extend(boxes[0])
-            continue
-        for key in box:
-            if key not in unlocked_indexes and box != boxes[len(boxes) - 1]:
+    for index in unlocked_indexes:
+        for key in boxes[index]:
+            if key not in unlocked_indexes:
                 unlocked_indexes.append(key)
-    return (len(unlocked_indexes) == len(boxes))
+    return (len(boxes) == len(unlocked_indexes))
