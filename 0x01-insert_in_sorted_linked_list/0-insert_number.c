411dc649cd69c942c66e70adfe9357082b182b43
Juansu01 <thejuank0@hotmail.com>
2021-12-15 08:29:06 -0500

    Adding function that insertes number in linked list

---
+++ 0x01-insert_in_sorted_linked_list/0-insert_number.c
@@ -0,0 +1,38 @@
+#include "lists.h"
+
+/**
+ * insert_node - adds a new node at the end of a listint_t list
+ * @head: pointer to pointer of first node of listint_t list
+ * @number: integer to be added
+ * Return: address of the new element or NULL if it fails
+ */
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *new;
+	listint_t *current;
+	unsigned int index = 0;
+
+	current = *head;
+
+	if (head == NULL)
+		return (NULL);
+
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = number;
+
+	while (current != NULL)
+	{
+		if (index == 4)
+		{
+			new->next = current->next;
+			current->next = new;
+			return (new);
+		}
+		current = current->next;
+		index++;
+	}
+
+	return (new);
+}
