ebc3595f749ceaa45926c274cc2104363cea67dc
Juansu01 <thejuank0@hotmail.com>
2022-01-27 09:20:15 -0500

    Adding module that computes stats

---
+++ 0x06-log_parsing/0-stats.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""Reads from sdtin and outputs stats."""
+
+from sys import stdin
+from collections import OrderedDict
+
+if __name__ == "__main__":
+    counter = 0
+    file_size = 0
+    status_codes = ["200", "301", "400", "401", "403", "404", "405", "500"]
+    ordered_codes = OrderedDict()
+    for code in status_codes:
+        ordered_codes[code] = 0
+    try:
+        for line in stdin:
+            try:
+                counter += 1
+                args = line.split()
+                if len(args) != 9:
+                    pass
+                file_size += int(args[8])
+                if args[7] in status_codes:
+                    ordered_codes[args[7]] += 1
+            except Exception:
+                pass
+            if counter % 10 == 0:
+                print("File size: {}".format(file_size))
+                for key, val in ordered_codes.items():
+                    if val != 0:
+                        print("{} :{}".format(key, val))
+    except KeyboardInterrupt:
+        print("File size: {}".format(file_size))
+        for key, val in ordered_codes.items():
+            if val != 0:
+                print("{} :{}".format(key, val))
+        raise
