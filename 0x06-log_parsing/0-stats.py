14e333b0e59185dd72822a731cc0776c49d215f5
Juansu01 <thejuank0@hotmail.com>
2022-01-27 11:28:55 -0500

    Using a normal dict this time

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -2,12 +2,11 @@
 """Reads from sdtin and outputs stats."""
 
 from sys import stdin
-from collections import OrderedDict
 
 
 def printit(size, _dict):
     print("File size: {}".format(size))
-    for key, val in _dict.items():
+    for key, val in sorted(_dict.items()):
         if val != 0:
             print("{} :{}".format(key, val))
 
@@ -16,7 +15,7 @@ if __name__ == '__main__':
     counter = 0
     file_size = 0
     status_codes = ["200", "301", "400", "401", "403", "404", "405", "500"]
-    ordered_codes = OrderedDict()
+    ordered_codes = {}
     for code in status_codes:
         ordered_codes[code] = 0
     try:
