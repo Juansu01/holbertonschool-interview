1757cb5f9a808f85b97d5b5bd3574b51998739fa
Juansu01 <thejuank0@hotmail.com>
2022-02-14 12:17:51 -0500

    Adding interview practice task

---
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+This module degfines a function that validates a data set
+which represents a UTF-8 encoding.
+"""
+
+
+def validUTF8(data):
+    """
+    Returns True if data is a valid UTF-8 enconding,
+    False otherwise.
+    """
+    counter = 0
+    for el in data:
+        if counter == 0:
+            if (el >> 5) == 0b110:
+                count = 1
+            elif (el >> 4) == 0b1110:
+                count = 2
+            elif (el >> 3) == 0b11110:
+                count = 3
+            elif (el >> 7):
+                return False
+        else:
+            if (el >> 6) != 0b10:
+                return False
+            counter -= 1
+    return (counter == 0)
