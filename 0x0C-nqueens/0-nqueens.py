46545539406fc329f018dde9bc47b2f77ba24387
Juansu01 <thejuank0@hotmail.com>
2022-07-20 17:35:05 -0500

    Add nqueens

---
+++ 0x0C-nqueens/0-nqueens.py
@@ -0,0 +1,69 @@
+#!/usr/bin/python3
+"""
+N Queens interview practice.
+"""
+
+import sys
+
+
+if len(sys.argv) != 2:
+    print("Usage: nqueens N")
+    sys.exit(1)
+
+N = sys.argv[1]
+result = []
+
+try:
+    N = int(N)
+except ValueError:
+    print("N must be a number")
+    sys.exit(1)
+
+if N < 4:
+    print("N must be at least 4")
+    sys.exit(1)
+
+
+class NQueenSolution:
+
+    def __init__(self, n):
+        self.n = n
+        self.x = [0 for i in range(n + 1)]
+        self.res = []
+
+    def place(self, k, i):
+        # j checks from 1 to k - 1 (Up to previous queen)
+        for j in range(1, k):
+            # There is already a queen in column
+            # or a queen in same diagonal
+            if self.x[j] == i or \
+               abs(self.x[j] - i) == abs(j - k):
+                return 0
+        return 1
+
+    def nQueen(self, k):
+        # i goes from column 1 to column n (1st column is 1st index)
+        for i in range(1, self.n + 1):
+            if self.place(k, i):
+                # Queen can be placed in i column
+                self.x[k] = i
+                if k == self.n:
+                    # Placed all 4 Queens (A solution was found)
+                    solution = []
+                    for i in range(1, self.n + 1):
+                        solution.append([i - 1, self.x[i] - 1])
+                    self.res.append(solution)
+                else:
+                    # Need to place more Queens
+                    self.nQueen(k + 1)
+        return self.res
+
+
+def main():
+    result = NQueenSolution(N)
+    result = result.nQueen(1)
+    for lis in result:
+        print(lis)
+
+
+main()
