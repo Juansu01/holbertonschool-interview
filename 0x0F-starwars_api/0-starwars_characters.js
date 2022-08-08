7b82a57acd0a40cc0dea04bfabad1dfef2428f55
Juansu01 <thejuank0@hotmail.com>
2022-08-08 17:07:18 -0500

    Complete interview practice

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -1,10 +1,18 @@
 #!/usr/bin/node
-
 const process = require('process');
 const request = require('request');
-
 const id = process.argv[2];
+let characterList = []
 
 request(`https://swapi-api.hbtn.io/api/films/${id}`, function(err, res, body) {
-    console.log(body);
+    let json = JSON.parse(body);
+    characterList = json.characters;
+
+    for (const char of characterList) {
+        request(char, function(err, res, body) {
+            let person = JSON.parse(body);
+            console.log(person.name);
+        });
+    }
+
 });
