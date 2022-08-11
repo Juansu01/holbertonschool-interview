898affe3fc481b6ff7b5e8f5c6bcd31f19e49ec3
Juansu01 <thejuank0@hotmail.com>
2022-08-11 15:15:44 -0500

    Add starwars chars

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -9,17 +9,24 @@ request(`https://swapi-api.hbtn.io/api/films/${id}`, function (err, res, body) {
     const json = JSON.parse(body);
     characterList = json.characters;
 
-    for (const char of characterList) {
-      request(char, function (err, res, body) {
-        if (err == null) {
-          const person = JSON.parse(body);
-          console.log(person.name);
-        } else {
-          console.error(err);
-        }
-      });
-    }
+    getCharacterName(0, characterList[0], characterList, characterList.length);
   } else {
     console.error(err);
   }
 });
+
+function getCharacterName (idx, url, characters, limit) {
+  if (idx === limit) {
+    return;
+  }
+  request(url, function (err, response, body) {
+    if (!err) {
+      const rbody = JSON.parse(body);
+      console.log(rbody.name);
+      idx++;
+      getCharacterName(idx, characters[idx], characters, limit);
+    } else {
+      console.error(err);
+    }
+  });
+}
