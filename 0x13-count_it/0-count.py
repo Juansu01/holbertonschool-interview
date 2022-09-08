711403a7cddd173b68b5f3aaff0e630bd3ced5b6
Juansu01 <thejuank0@hotmail.com>
2022-09-08 10:43:29 -0500

    Add count it

---
+++ 0x13-count_it/0-count.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+"""
+Interview practice
+"""
+
+import requests
+
+
+def count_words(subreddit, word_list, after=None, directory=None):
+    """
+    Queries the Reddit API, parses the title of all hot articles,
+    and prints a sorted count of given keywords.
+    """
+
+    if directory is None:
+        directory = dict()
+        for word in word_list:
+            directory[word] = 0
+
+    if after is None:
+        word_list = [[key, value] for key, value in directory.items()]
+        word_list = sorted(word_list, key=lambda x: (-x[1], x[0]))
+
+        for word in word_list:
+            if word[1]:
+                print(f"{word[0].lower()}: {word[1]}")
+
+        return None
+
+    reddit_string = "https://www.reddit.com/"
+    url = f"{reddit_string}r/{subreddit}/hot/.json"
+    params = {'limit': 100, 'after': after}
+    head = {'user-agent': 'my-app/0.0.1'}
+
+    res = requests.get(url, headers=head, params=params, allow_redirects=False)
+
+    if res.status_code != 200:
+        return None
+
+    try:
+        json = res.json()
+    except Exception:
+        return None
+
+    try:
+        data = json.get()
+        after, children = data.get("after"), data.get("children")
+
+        for child in children:
+            title, post = child.get("title"), child.get("data")
+            to_lower = [string.lower() for string in title.split(' ')]
+
+            for word in word_list:
+                directory[word] += to_lower.count(word.lower())
+    except Exception:
+        return None
+
+    count_words(subreddit, word_list, after, directory)
