a5e1c09c19330c5e5316d51694418300a7cd89aa
Juansu01 <thejuank0@hotmail.com>
2022-10-13 10:41:59 -0500

    Add: Merge sort

---
+++ 0x18-merge_sort/0-merge_sort.c
@@ -0,0 +1,153 @@
+#include "sort.h"
+
+/**
+ * copy_buffer - Copies from source into dest
+ *
+ * @src: source buffer
+ * @dest: destination buffer
+ * @size: size of buffers
+ * Return: Doesn't return
+ */
+void copy(int *src, int *dest, int size)
+{
+	int i;
+
+	for (i = 0; i < size; i++)
+    {
+        dest[i] = src[i];
+    }
+
+}
+
+/**
+ * merge - merges two sorted arrays in ascending order
+ * @array: first set of data
+ * @buff: second set of data
+ * @minL: lower range of first set of data
+ * @maxL: upper range of first set of data
+ * @minR: lower range of second set of data
+ * @maxR: upper range of second set of data
+ *
+ * Return: Doesn't return
+ */
+void _merge(int *array, int *buff, int minL, int maxL, int minR, int maxR)
+{
+	int i = minL, j = minR, k = minL;
+
+	while (i <= maxL || j <= maxR)
+    {
+		if (i <= maxL && j <= maxR)
+        {
+			if (buff[i] <= buff[j])
+				array[k] = buff[i], k++, i++;
+			else
+				array[k] = buff[j], k++, j++;
+        }
+		else if (i > maxL && j <= maxR)
+        {
+			array[k] = buff[j], k++, j++;
+        }
+		else
+        {
+			array[k] = buff[i], k++, i++;
+        }
+    }
+}
+
+/**
+ * print_range - Takes a range and prints values inside that range
+ * @array: Array with values to be printed
+ * @beginning: Where the range starts
+ * @end: Where the range ends
+ *
+ * Return: Doesn't return
+ */
+void print_range(int *array, int beginning, int end)
+{
+	int i;
+
+	for (i = beginning; i <= end; i++)
+	{
+		if (i > beginning)
+        {
+			printf(", ");
+        }
+		printf("%d", array[i]);
+	}
+	printf("\n");
+}
+
+/**
+ * my_merge_sort - Splits data into merge tree
+ *
+ * @array: Array of data to be split
+ * @buff: Auxiliary array of data for merging
+ * @min: Min range of data in array
+ * @max: Max range of data in array
+ * @size: size of total data
+ *
+ * Return: Doesn't return
+ */
+void my_merge_sort(int *array, int *buff, int min, int max, int size)
+{
+	int mid, tmax, minL, maxL, minR, maxR;
+
+	if ((max - min) <= 0)
+    {
+		return;
+    }
+
+	mid = (max + min + 1) / 2;
+	tmax = max;
+	max = mid - 1;
+	minL, maxL = min, max;
+	split(array, buff, min, max, size);
+
+	min, max = mid, tmax;
+	minR, maxR = min, max;
+	split(array, buff, min, max, size);
+
+	printf("Merging...\n");
+	printf("[left]: ");
+
+	print_range(array, minL, maxL);
+	printf("[right]: ");
+
+	print_range(array, minR, maxR);
+	_merge(array, buff, minL, maxL, minR, maxR);
+	copy(array, buff, size);
+
+	printf("[Done]: ");
+	print_range(array, minL, maxR);
+}
+
+/**
+ * merge_sort - sorts an array of integers in ascending order
+ * using the Merge sort algorithm
+ *
+ * @array: array of data to be sorted
+ * @size: size of data
+ *
+ * Return: No Return
+ */
+void merge_sort(int *array, size_t size)
+{
+	int *buff;
+
+	if (size < 2)
+    {
+		return;
+    }
+
+	buff = malloc(sizeof(int) * size);
+	if (!buff)
+    {
+		return;
+    }
+
+	copy(array, buff, size);
+
+	my_merge_sort(array, buff, 0, size - 1, size);
+
+	free(buff);
+}
